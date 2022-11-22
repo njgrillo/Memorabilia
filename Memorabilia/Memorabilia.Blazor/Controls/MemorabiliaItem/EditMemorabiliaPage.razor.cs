82389357186a83c09aa961939d7db24c3c7eaa95
Nick Grillo <nick.grillo@surepassid.com>
2022-11-22 13:59:25 -0500

    commit

---
+++ Memorabilia/Memorabilia.Blazor/Controls/MemorabiliaItem/EditMemorabiliaPage.razor.cs
@@ -0,0 +1,28 @@
+﻿#nullable disable
+
+namespace Memorabilia.Blazor.Controls.MemorabiliaItem;
+
+public partial class EditMemorabiliaPage<TItem> where TItem : MemorabiliaItemEditViewModel
+{
+    [Parameter]
+    public RenderFragment Content { get; set; }
+
+    [Parameter]
+    public TItem Item { get; set; }
+
+    [Parameter]
+    public EventCallback OnLoad { get; set; }
+
+    [Parameter]
+    public EventCallback<TItem> OnSave { get; set; }    
+
+    protected async Task Load()
+    {
+        await OnLoad.InvokeAsync();
+    }
+
+    protected async Task Save()
+    {
+        await OnSave.InvokeAsync();
+    }
+}
