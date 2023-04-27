48ac2ee9c98b521552beb4d5a8c4cf3afe10ea55
Nick Grillo <nick.grillo@surepassid.com>
2023-04-27 15:15:43 -0400

    commit

--- Memorabilia/Memorabilia.Application/Features/User/Settings/UserSettingsViewModel.cs
+++ Memorabilia/Memorabilia.Application/Features/User/Settings/UserSettingsViewModel.cs
@@ -5,4 +5,6 @@ public class UserSettingsViewModel : ViewModel
     public UserSettingsViewModel() { }
 
     public override string PageTitle => "User Settings";
+
+    public string DashboardImageFileName => Domain.Constants.ImageFileName.Dashboard;
 }
