766123e7b90d6c9be832b3b733651d1245cb7c4c
Nick Grillo <nick.grillo@surepassid.com>
2023-06-14 15:54:47 -0400

    commit

---
+++ Memorabilia/Memorabilia.Application/Extensions/UserExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Memorabilia.Application.Extensions;
+
+public static class UserExtensions
+{
+    public static bool IsAdmin(this Entity.User user)
+        => user != null && user.UserRoleId == Constant.Role.Admin.Id;
+}
---
+++ Memorabilia/Memorabilia.Web/Shared/NavMenu.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace Memorabilia.Web.Shared;
+
+public partial class NavMenu
+{
+    [Inject]
+    public IApplicationStateService ApplicationStateService { get; set; }
+}
