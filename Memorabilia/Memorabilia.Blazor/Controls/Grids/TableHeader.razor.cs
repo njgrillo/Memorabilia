8b0901ab4a0b5bca754c1e879dcb03f415824b3f
Nick Grillo <nick.grillo@surepassid.com>
2023-10-30 09:52:52 -0400

    commit

---
+++ Memorabilia/Memorabilia.Blazor/Controls/Grids/TableHeader.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace Memorabilia.Blazor.Controls.Grids;
+
+public partial class TableHeader
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    [Parameter]
+    public bool Visible { get; set; }
+        = true;
+}
