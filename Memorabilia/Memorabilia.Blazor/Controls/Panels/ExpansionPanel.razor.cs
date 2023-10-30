f0ff0329c1cc672816052bff060d757c7ca89104
Nick Grillo <nick.grillo@surepassid.com>
2023-10-30 09:00:57 -0400

    commit

---
+++ Memorabilia/Memorabilia.Blazor/Controls/Panels/ExpansionPanel.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace Memorabilia.Blazor.Controls.Panels;
+
+public partial class ExpansionPanel
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }    
+
+    [Parameter]
+    public bool Expanded { get; set; }
+        = true;
+
+    [Parameter]
+    public string Text { get; set; }
+}
