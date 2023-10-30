c93ae98049557fe2bf75cd44ca717da3de96ab1b
Nick Grillo <nick.grillo@surepassid.com>
2023-10-30 13:40:16 -0400

    commit

---
+++ Memorabilia/Memorabilia.Blazor/Controls/Grids/Buttons/NavigationGridButton.razor.cs
@@ -0,0 +1,22 @@
+﻿namespace Memorabilia.Blazor.Controls.Grids.Buttons;
+
+public partial class NavigationGridButton
+{
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    [Parameter]
+    public Color Color { get; set; }
+        = Color.Primary;
+
+    [Parameter]
+    public string NavigationPath { get; set; }
+
+    [Parameter]
+    public string StartIcon { get; set; }
+        = Icons.Material.Outlined.ModeEditOutline;
+
+    [Parameter]
+    public string TooltipText { get; set; }
+        = "Edit";
+}
