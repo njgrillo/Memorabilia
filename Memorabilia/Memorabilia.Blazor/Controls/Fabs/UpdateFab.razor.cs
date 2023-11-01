1bd3a59068a46bafb1a2ea245df513c0df157e66
Nick Grillo <nick.grillo@surepassid.com>
2023-11-01 12:20:34 -0400

    commit

--- Memorabilia/Memorabilia.Blazor/Controls/Fabs/UpdateFab.razor.cs
+++ Memorabilia/Memorabilia.Blazor/Controls/Fabs/UpdateFab.razor.cs
@@ -5,6 +5,10 @@ public partial class UpdateFab
     [Parameter]
     public EventCallback OnUpdate { get; set; }
 
+    [Parameter]
+    public bool Visible { get; set; }
+        = true;
+
     public async Task Update()
     {
         await OnUpdate.InvokeAsync();
