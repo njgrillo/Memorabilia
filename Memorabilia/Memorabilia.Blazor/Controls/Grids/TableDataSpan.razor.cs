bb271ac89b10394062d7d08aaa9fca192681a006
Nick Grillo <nick.grillo@surepassid.com>
2023-11-01 12:52:11 -0400

    commit

---
+++ Memorabilia/Memorabilia.Blazor/Controls/Grids/TableDataSpan.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace Memorabilia.Blazor.Controls.Grids;
+
+public partial class TableDataSpan
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    [Parameter]
+    public string Colspan { get; set; }
+        = "17";
+}
