ffeabf8f59adbe2825470936c9dc54c1ce686911
Nick Grillo <nick.grillo@surepassid.com>
2023-11-10 14:40:25 -0500

    commit

--- Memorabilia/Memorabilia.Blazor/Controls/Tabs/TabPanel.razor.cs
+++ Memorabilia/Memorabilia.Blazor/Controls/Tabs/TabPanel.razor.cs
@@ -12,11 +12,17 @@ public partial class TabPanel
     public RenderFragment ChildContent { get; set; }
 
     [Parameter]
+    public string Class { get; set; }
+
+    [Parameter]
     public bool DisplayBadge { get; set; }
 
     [Parameter]
     public bool Expanded { get; set; }
         = true;
+
+    [Parameter]
+    public string Style { get; set; }
 
     [Parameter]
     public string Text { get; set; }
