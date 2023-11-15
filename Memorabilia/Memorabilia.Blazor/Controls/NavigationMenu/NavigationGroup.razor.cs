77fbdd1f589a745700a758e6b7007de28c0e9226
Nick Grillo <nick.grillo@surepassid.com>
2023-11-15 08:45:38 -0500

    commit

---
+++ Memorabilia/Memorabilia.Blazor/Controls/NavigationMenu/NavigationGroup.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace Memorabilia.Blazor.Controls.NavigationMenu;
+
+public partial class NavigationGroup
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    [Parameter]
+    public string Icon { get; set; }
+
+    [Parameter]
+    public string Title { get; set; }
+
+    [Parameter]
+    public bool Visible { get; set; }
+        = true;
+}
---
+++ Memorabilia/Memorabilia.Blazor/Controls/NavigationMenu/NavigationLink.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace Memorabilia.Blazor.Controls.NavigationMenu;
+
+public partial class NavigationLink
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    [Parameter]
+    public string Href { get; set; }
+
+    [Parameter]
+    public string Icon { get; set; }
+
+    [Parameter]
+    public bool Visible { get; set; }
+        = true;
+}
