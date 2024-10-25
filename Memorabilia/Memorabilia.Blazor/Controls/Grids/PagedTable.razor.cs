4a8b3b2d68d6e36386d45f0d94e9bc047df7239f
Nick Grillo <njgrillo@gmail.com>
2024-10-25 11:14:18 -0400

    commit

---
+++ Memorabilia/Memorabilia.Blazor/Controls/Grids/PagedTable.razor.cs
@@ -0,0 +1,50 @@
+﻿namespace Memorabilia.Blazor.Controls.Grids;
+
+public partial class PagedTable<TItem> where TItem : class
+{
+    [Parameter]
+    public bool Bordered { get; set; }
+
+    [Parameter]
+    public RenderFragment Columns { get; set; }
+
+    [Parameter]
+    public bool Dense { get; set; }
+
+    [Parameter]
+    public bool Hover { get; set; }
+        = true;
+
+    [Parameter]
+    public List<TItem> Items { get; set; }
+        = [];
+
+    [Parameter]
+    public Func<TableState, Task<TableData<TItem>>> OnRead { get; set; }
+
+    [Parameter]
+    public RenderFragment Rows { get; set; }
+
+    [Parameter]
+    public bool Striped { get; set; }
+        = true;
+
+    [Parameter]
+    public string Title { get; set; }
+        = "Records";
+
+    [Parameter]
+    public bool Virtualize { get; set; }
+        = true;
+
+    protected string Search { get; private set; }
+
+    private MudTable<TItem> _table = new();
+
+    protected virtual void Filter(string search)
+    {
+        Search = search;
+
+        _table.ReloadServerData();
+    }
+}
