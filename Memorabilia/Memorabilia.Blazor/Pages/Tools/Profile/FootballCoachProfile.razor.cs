d1183c986779eeee846467190c4418109b93152c
Nick Grillo <njgrillo@gmail.com>
2024-11-04 11:56:15 -0500

    commit

---
+++ Memorabilia/Memorabilia.Blazor/Pages/Tools/Profile/FootballCoachProfile.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace Memorabilia.Blazor.Pages.Tools.Profile;
+
+public partial class FootballCoachProfile
+{
+    private string _expandToggleText
+        = "Expand All";
+
+    private bool _isExpanded;
+
+    private void ToggleExpand()
+    {
+        _isExpanded = !_isExpanded;
+
+        _expandToggleText =
+            _isExpanded
+                ? "Collapse All"
+                : "Expand All";
+    }
+}
