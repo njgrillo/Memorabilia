ae25515930f44461b42bc5b08f22f1245fa043b2
Nick Grillo <njgrillo@gmail.com>
2024-12-17 16:44:42 -0500

    commit

--- Memorabilia/Memorabilia.Domain/Constants/InternationalHallOfFameType.cs
+++ Memorabilia/Memorabilia.Domain/Constants/InternationalHallOfFameType.cs
@@ -2,6 +2,7 @@
 
 public sealed class InternationalHallOfFameType : DomainItemConstant
 {
+    public static readonly InternationalHallOfFameType AustralianHallOfFame = new(9, "Australian Hall of Fame");
     public static readonly InternationalHallOfFameType CanadianHallOfFame = new(1, "Canadian Hall of Fame");
     public static readonly InternationalHallOfFameType CaribbeanHallOfFame = new(2, "Caribbean Hall of Fame");
     public static readonly InternationalHallOfFameType CubanHallOfFame = new(7, "Cuban Hall of Fame");
@@ -11,6 +12,7 @@ public sealed class InternationalHallOfFameType : DomainItemConstant
     
     public static readonly InternationalHallOfFameType[] All =
     [
+        AustralianHallOfFame,
         CanadianHallOfFame,
         CaribbeanHallOfFame,
         CubanHallOfFame,
